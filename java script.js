0904e2c33b82dfc71f3dfc83e9e835e0b8a06bc7
ecosportokutimur-blip <ecosportokutimur@gmail.com>
2026-07-12 08:04:28 +0700

    Update java script.js

--- java script.js
+++ java script.js
@@ -1,7 +1,1 @@
-// Buka database yang sama
-var db = await openDB();
-var record = await dbGet(dbId); // dbId dari metadata yang di-copy
-if (record && record.file) {
-  var url = URL.createObjectURL(record.file);
-  // Gunakan url untuk img src atau video src
-}
+
